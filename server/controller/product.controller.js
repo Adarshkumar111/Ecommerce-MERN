05797ef318b5687b888f78f7f341b8fc489ab23b
Adarshkumar111 <adarsh.kumar232@lpu.in>
2026-05-09 13:09:16 +0530

    get all prod, update prodd, update images complete

--- server/controller/product.controller.js
+++ server/controller/product.controller.js
@@ -1,96 +1,189 @@
 import { Product } from "../model/Product.js";
 import bufferGenerator from "../utils/bufferGenerator.js";
 import TryCatch from "../utils/TryCatch.js";
-import cloudinary from "cloudinary"
+import cloudinary from "cloudinary";
 
-export const createProduct = TryCatch(async(req, res)=>{
-    if(req.user.role !=="admin") return res.status(403).json({
-        message: "You are not  admin"
-    })
+export const createProduct = TryCatch(async (req, res) => {
+  if (req.user.role !== "admin")
+    return res.status(403).json({
+      message: "You are not  admin",
+    });
 
-    const {title, about, category, price, stock}=req.body
+  const { title, about, category, price, stock } = req.body;
 
-    const files= req.files
+  const files = req.files;
 
+  if (!files || files.length === 0)
+    return res.status(400).json({
+      message: "Please upload at least one image",
+    });
 
-    if(!files || files.length === 0) return res.status(400).json({
-        message:"Please upload at least one image"
-    })
+  const imagesUploadPromises = files.map(async (file) => {
+    const fileBuffer = bufferGenerator(file);
 
-    const imagesUploadPromises = files.map(async(file)=>{
-        const fileBuffer = bufferGenerator(file)
+    const result = await cloudinary.v2.uploader.upload(fileBuffer.content);
 
-        const result =await cloudinary.v2.uploader.upload(fileBuffer.content)
+    return {
+      id: result.public_id,
+      url: result.secure_url,
+    };
+  });
 
-        return {
-            id: result.public_id,
-            url: result.secure_url,
-        };
-    })
+  const uploadImage = await Promise.all(imagesUploadPromises);
 
-    const uploadImage = await Promise.all(imagesUploadPromises)
+  const product = await Product.create({
+    title,
+    about,
+    category,
+    price,
+    stock,
+    images: uploadImage,
+  });
 
-    const product =  await Product.create({
-        title,
-        about,
-        category,
-        price,
-        stock,
-        images: uploadImage
-    })
-
-    res.status(201).json({
-        message:"Product created successfully",
-        product
-    })
-})
+  res.status(201).json({
+    message: "Product created successfully",
+    product,
+  });
+});
 
 // get all products
-export const getAllProducts= TryCatch(async(req, res)=>{
-    const {search, category, page, sortByPrice}=req.query;
+export const getAllProducts = TryCatch(async (req, res) => {
+  const { search, category, page, sortByPrice } = req.query;
 
-    const filter={};
+  const filter = {};
 
-    if(search){
-        filter.title={
-            $regex: search,
-            $options:"i",
-        };
+  if (search) {
+    filter.title = {
+      $regex: search,
+      $options: "i",
+    };
+  }
+  if (category) {
+    filter.category = category;
+  }
+
+  const limit = 8;
+  const skip = (page - 1) * limit;
+  let sortOptions = { createdAt: -1 };
+  if (sortByPrice) {
+    if (sortByPrice === "lowToHigh") {
+      sortOptions = { price: 1 };
+    } else if (sortByPrice === "highToLow") {
+      sortOptions = { price: -1 };
     }
-    if(category){
-        filter.category=category;
+  }
+
+  const products = await Product.find(filter)
+    .sort(sortOptions)
+    .limit(limit)
+    .skip(skip);
+
+  const categories = await Product.distinct("category");
+
+  const newProduct = await Product.find().sort("-createdAt").limit(8);
+
+  const countProduct = await Product.countDocuments();
+
+  const totalPages = Math.ceil(countProduct / limit);
+
+  res.status(200).json({
+    products,
+    categories,
+    newProduct,
+    totalPages,
+  });
+});
+
+// get single product
+
+export const getSingleProduct = TryCatch(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  const relatedProduct = await Product.find({
+    category: product.category,
+    _id: { $ne: product._id },
+  }).limit(4);
+
+  res.json({ product, relatedProduct });
+});
+
+// update product
+
+export const updateProduct = TryCatch(async (req, res) => {
+  if (req.user.role !== "admin")
+    return res.status(403).json({
+      message: "You are not  admin",
+    });
+
+  const { title, about, category, price, stock } = req.body;
+  const updateFields = {};
+
+  if (title) updateFields.title = title;
+  if (about) updateFields.about = about;
+  if (stock) updateFields.stock = stock;
+  if (price) updateFields.price = price;
+  if (category) updateFields.category = category;
+
+  const updateProduct = await Product.findByIdAndUpdate(
+    req.params.id,
+    updateFields,
+    { new: true, runValidators: true },
+  );
+  if (!updateProduct)
+    return res.status(404).json({
+      message: "Product not found",
+    });
+
+  res.json({
+    message: "Product Updated sucessful",
+    updateProduct,
+  });
+});
+
+
+// update product image
+
+export const updateProductImage = TryCatch(async (req, res) => {
+  if (req.user.role !== "admin")
+    return res.status(403).json({
+      message: "You are not  admin",
+    });
+
+  const { id } = req.params;
+  const files = req.files;
+  if (!files || files.length === 0)
+    return res.status(400).json({
+      message: "Please upload at least one image",
+    });
+
+  const product = await Product.findById(id);
+  if (!product)
+    return res.status(404).json({
+      message: "Product not found",
+    });
+  const oldImages = product.images || [];
+  for (const img of oldImages) {
+    if (img.id) {
+      await cloudinary.v2.uploader.destroy(img.id);
     }
+  }
+  const imagesUploadPromises = files.map(async (file) => {
+    const fileBuffer = bufferGenerator(file);
 
-    const limit=8;
-    const skip=(page - 1) * limit;
-    let sortOptions={createdAt: -1}
-    if(sortByPrice){
-        if(sortByPrice === "lowToHigh"){
-            sortOptions={price:1}
-        }
-        else if(sortByPrice === "highToLow"){
-            sortOptions={price: -1}
-        }
-    }
+    const result = await cloudinary.v2.uploader.upload(fileBuffer.content);
 
-    const products = await Product.find(filter)
-  .sort(sortOptions)
-  .limit(limit)
-  .skip(skip);
+    return {
+      id: result.public_id,
+      url: result.secure_url,
+    };
+  });
 
-    const categories= await Product.distinct("category");
-
-    const newProduct= await Product.find().sort("-createdAt").limit(8);
-
-    const countProduct = await Product.countDocuments()
-
-    const totalPages = Math.ceil(countProduct/limit)
-
-    res.status(200).json({
-        products,
-        categories,
-        newProduct,
-        totalPages
-    })
-
-})
+  const uploadImage = await Promise.all(imagesUploadPromises);
+  product.images = uploadImage;
+  await product.save();
+  
+  res.status(200).json({
+    message: "Product image updated successfully",
+    product,
+  });
+});
--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { isAuth } from '../middleware/isAuth.middleware.js';
-import { createProduct, getAllProducts } from '../controller/product.controller.js';
+import { createProduct, getAllProducts, getSingleProduct, updateProduct, updateProductImage } from '../controller/product.controller.js';
 import uploadFiles from '../middleware/multer.middleware.js';
 
 const router = express.Router();
@@ -8,7 +8,10 @@ const router = express.Router();
 
 router.post("/product/new", isAuth,uploadFiles, createProduct)
 router.get("/product/all", getAllProducts)
+router.get("/product/:id", getSingleProduct)
+router.put("/product/:id",isAuth, updateProduct)
+router.post("/product/:id",isAuth,uploadFiles, updateProductImage)
+
 
 export default router;
 
-//14 min lec 4
